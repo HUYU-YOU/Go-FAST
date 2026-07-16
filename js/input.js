df294a44e8a732d6d2089ca12699a782844383e1
Hu man <romaincontant.80@gmail.com>
2026-07-16 13:35:30 +0100

    Update input.js

--- js/input.js
+++ js/input.js
@@ -1,4 +1,4 @@
-const keys = { up: false, down: false, left: false, right: false, nitro: false, enter: false, esc: false };
+const keys = { up: false, down: false, left: false, right: false, nitro: false, shoot: false, enter: false, esc: false };
 
 window.addEventListener('keydown', (e) => {
     let k = e.key.toLowerCase();
@@ -7,6 +7,7 @@ window.addEventListener('keydown', (e) => {
     if(k === 'arrowleft' || k === 'q' || k === 'a') keys.left = true;
     if(k === 'arrowright' || k === 'd') keys.right = true;
     if(e.key === ' ') { keys.nitro = true; e.preventDefault(); }
+    if(k === 'f' || e.key === 'Shift') { keys.shoot = true; e.preventDefault(); }
     if(e.key === 'Enter') { keys.enter = true; e.preventDefault(); }
     if(e.key === 'Escape') { keys.esc = true; e.preventDefault(); }
 });
@@ -18,6 +19,7 @@ window.addEventListener('keyup', (e) => {
     if(k === 'arrowleft' || k === 'q' || k === 'a') keys.left = false;
     if(k === 'arrowright' || k === 'd') keys.right = false;
     if(e.key === ' ') keys.nitro = false;
+    if(k === 'f' || e.key === 'Shift') keys.shoot = false;
     if(e.key === 'Enter') keys.enter = false;
     if(e.key === 'Escape') keys.esc = false;
 });
